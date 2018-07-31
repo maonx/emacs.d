46eca335967139b427c6ce995d8b5699885b3aa0
Maonx <imaonx@gmail.com>
2018-07-31 22:58:53 +0800

    add evil mode

--- init.el
+++ init.el
@@ -1,9 +1,35 @@
+(require 'package)
+
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;;; init screen
+(setq inhibit-startup-message t
+      initial-scratchq-message ""
+      initial-major-mode 'fundamental-mode
+      inhibit-splash-screen t)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages (quote (evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
