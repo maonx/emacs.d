af5b5ba4ac473fb95de94d6ced1691d1d1c1862e
Maonx <imaonx@gmail.com>
2019-12-02 17:13:02 +0800

    update

--- lisp/custom.el
+++ lisp/custom.el
@@ -3,9 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("728eda145ad16686d4bbb8e50d540563573592013b10c3e2defc493f390f7d83" "2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" default)))
  '(package-selected-packages
    (quote
-    (doom-modeline all-the-icons-dired spaceline-all-the-icons projectile cnfonts evil-escape evil-org web-mode use-package php-mode key-chord evil-nerd-commenter evil-matchit evil-magit evil-leader doom-themes dashboard counsel company all-the-icons))))
+    (mood-line doom-modeline all-the-icons-dired spaceline-all-the-icons projectile cnfonts evil-escape evil-org web-mode use-package php-mode key-chord evil-nerd-commenter evil-matchit evil-magit evil-leader doom-themes dashboard counsel company all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -5,7 +5,6 @@
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-;; (set-frame-font "等距更纱黑体 SC 13" nil t)
 ;; Hide tool bar
 (tool-bar-mode -1)
 
@@ -32,23 +31,47 @@
 	     :config
 	     (load-theme 'doom-one-light t))
 
-;; (use-package dashboard
-;;   :ensure t
-;;   :config
-;;   (dashboard-setup-startup-hook)
-;;   ;; Content is not centered by default. To center, set
-;;   (setq dashboard-center-content t)
-;;   (setq dashboard-set-footer nil)
-;;   (setq dashboard-items '((recents  . 5)
-;; 			  (projects . 5)
-;; 			  (agenda . 5)
-;; 			  (bookmarks . 5)
-;; 			  (registers . 5)))
-;;   (setq dashboard-set-heading-icons t)
-;;   (setq dashboard-set-file-icons t)
-;;   )
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  ;; Content is not centered by default. To center, set
+  (setq dashboard-center-content t)
+  (setq dashboard-set-footer nil)
+  (setq dashboard-items '((recents  . 5)
+			  (projects . 5)
+			  (agenda . 5)
+			  (bookmarks . 5)
+			  ))
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  )
 
 (use-package page-break-lines
   :ensure t)
+
+(setq inhibit-compacting-font-caches t)
+(use-package all-the-icons
+  :ensure t
+  :defer t  
+  :init
+  (set-frame-font "all-the-icons" t)
+  :config
+  (set-frame-font "Monaco" t)
+  )
+
+(use-package doom-modeline
+  :ensure t
+  :hook (after-init . doom-modeline-mode))
+
+;; Setting English Font
+(set-face-attribute 'default nil :font "Monaco 10")
+
+;; Chinese Font
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font (frame-parameter nil 'font)
+                    charset (font-spec :family "Microsoft Yahei"
+                                       :size 12.0)))
+
 
 (provide 'init-ui)
